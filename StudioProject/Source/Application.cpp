714b74c058f5598c3d3dcad66aaa403a6bcde236
Herophillix <42328650+Herophillix@users.noreply.github.com>
2019-08-15 16:44:48 +0800

    New

--- StudioProject/Source/Application.cpp
+++ StudioProject/Source/Application.cpp
@@ -125,7 +125,7 @@ void Application::Init()
 void Application::Run()
 {
 	//Main Loop
-	Scene *scene = new SceneMaze;
+	Scene *scene = new SceneMole;
 	scene->Init();
 
 	m_timer.startTimer();    // Start timer to calculate how long it takes to render this frame
